43b846b4254ad54d4f11bad85bc376dafe9edadc
Elif İrem Kara Kadyrov <83959541+elfrmkr@users.noreply.github.com>
2024-11-14 11:23:00 +0100

    feat: added mutable states to hold values for composable elements.

--- app/src/main/java/eu/tutorials/unitconverter/MainActivity.kt
+++ app/src/main/java/eu/tutorials/unitconverter/MainActivity.kt
@@ -23,6 +23,10 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -50,6 +54,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter() {
+
+    //setting up states
+    var inputValue by remember{mutableStateOf(value = "") }
+    var outputValue by remember{mutableStateOf(value = "") }
+    var inputUnit by remember{mutableStateOf(value = "Centimetres") }
+    var outputUnit  by remember{mutableStateOf(value = "Metres") }
+    var iExpanded by remember { mutableStateOf(value=false) }
+    var oExpanded by remember { mutableStateOf(value=false) }
+    var conversionFactor = remember {mutableStateOf(value = 0.01) }
+
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -58,8 +73,8 @@ fun UnitConverter() {
         //UI elements will be stacked below each other
             Text("Unit Converter")
             Spacer(modifier = Modifier.height(16.dp))
-            OutlinedTextField(value = "Enter Value" , onValueChange = {
-
+            OutlinedTextField(value = inputValue , onValueChange = {
+                inputValue = it
         })
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -71,7 +86,7 @@ fun UnitConverter() {
                     Icon(Icons.Default.ArrowDropDown,
                         contentDescription = "Arrow Down")
                 }
-                DropdownMenu(expanded = true, onDismissRequest = { /*TODO*/ }) {
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
                     DropdownMenuItem(
                         text = { Text(text = "Centimetres") },
                         onClick = { /*TODO*/ }
@@ -98,7 +113,7 @@ fun UnitConverter() {
                     Icon(Icons.Default.ArrowDropDown,
                         contentDescription = "Arrow Down")
                 }
-                DropdownMenu(expanded = true, onDismissRequest = { /*TODO*/ }) {
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
                     DropdownMenuItem(
                         text = { Text(text = "Centimetres") },
                         onClick = { /*TODO*/ }
